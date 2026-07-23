4174722f9e54d0605d5f4c0f884e1bcbc276c694
Patrick de Sá <patrickdesainfo@gmail.com>
2026-07-23 12:46:09 +0100

    add cron and schedule for sendings emails

--- comunicacao_api/src/main/java/com/luizalebs/comunicacao_api/business/service/EmailService.java
+++ comunicacao_api/src/main/java/com/luizalebs/comunicacao_api/business/service/EmailService.java
@@ -2,6 +2,7 @@ package com.luizalebs.comunicacao_api.business.service;
 
 import com.luizalebs.comunicacao_api.api.dto.ComunicacaoOutDTO;
 import com.luizalebs.comunicacao_api.api.dto.EmailRequestDTO;
+import com.luizalebs.comunicacao_api.business.converter.EmailRequestDTOConverter;
 import com.luizalebs.comunicacao_api.infraestructure.clients.EmailClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,11 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     private final EmailClient emailClient;
+    private final EmailRequestDTOConverter emailConverter;
+    private final ComunicacaoService comunicacaoService;
 
     public void enviaEmail(EmailRequestDTO dto) {
         emailClient.enviarEmail(dto);
     }
+
 }
--- comunicacao_api/src/main/java/com/luizalebs/comunicacao_api/infraestructure/repositories/ComunicacaoRepository.java
+++ comunicacao_api/src/main/java/com/luizalebs/comunicacao_api/infraestructure/repositories/ComunicacaoRepository.java
@@ -1,9 +1,19 @@
 package com.luizalebs.comunicacao_api.infraestructure.repositories;
 
 import com.luizalebs.comunicacao_api.infraestructure.entities.ComunicacaoEntity;
+import com.luizalebs.comunicacao_api.infraestructure.enums.StatusEnvioEnum;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.repository.CrudRepository;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
 public interface ComunicacaoRepository extends CrudRepository<ComunicacaoEntity, Long> {
 
     ComunicacaoEntity findByEmailDestinatario(String nomeDestinatario);
+
+    List<ComunicacaoEntity> findByStatusEnvioAndDataHoraEnvioBetween(StatusEnvioEnum statusEnvio,
+                                                                LocalDateTime dataInicial,
+                                                                LocalDateTime dataFinal);
 }
